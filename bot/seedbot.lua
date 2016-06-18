3e0298a3ce6282faf5c99f7d97f7438de232a50f
hossein101 <hossein_lotg@yahoo.com>
2016-06-18 18:08:50 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -232,7 +232,7 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {224345655,234445563,94405642(our_id)},--Sudo users
+    sudo_users = {224345655,234445563,94405642},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Red-Bot v4
 An advanced administration bot based on TG-CLI written in Lua
